dcc725c9fb06b15f65376b989847d8c84b3b4330
Distant_Travellr <distant_travellr@pop-os.localdomain>
2021-12-31 11:51:41 +1030

    more stuff

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use serde_bencode::ser;
 use serde_bencoded::from_bytes;
 use std::io::Read;
 use torrent_struct::Torrent;
+use url::Url;
 mod torrent_struct;
 use crypto::digest::Digest;
 use crypto::sha1::Sha1;
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let bytes = if atty::is(atty::Stream::Stdin) {
-        let mut file = std::fs::File::open("./sample.torrent")?;
+        let mut file = std::fs::File::open("./sintel.torrent")?;
         let mut buf = Vec::new();
         file.read_to_end(&mut buf)?;
         buf
@@ -25,10 +27,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             println!("file length:\t{}", f.length);
         }
     }
+    println!("announce_url {:?}", torrent.announce);
     let info_bytes: Vec<u8> = ser::to_bytes(&torrent.info)?;
     let mut hasher = Sha1::new();
     hasher.input(&info_bytes);
     let info_hash = hasher.result_str();
     println!("infohash {:?}", &info_hash);
+    let val: &str = &*torrent.announce;
+    let parsed_url = Url::parse(val)?;
+    println!(
+        "{:?}{:?}",
+        parsed_url.domain().unwrap(),
+        parsed_url.port().unwrap()
+    );
+
+    let uri = parsed_url.domain().unwrap();
+    println!("{}", uri);
+
+    let body = reqwest::get(format!("http://{}", uri))
+        .await?
+        .text()
+        .await?;
+
+    println!("body = {:?}", body);
     Ok(())
 }
--- src/torrent_struct.rs
+++ src/torrent_struct.rs
@@ -1,6 +1,6 @@
 use serde_bytes::ByteBuf;
 use serde_derive::{Deserialize, Serialize};
-extern crate serde_derive;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Node(String, i64);
@@ -22,7 +22,7 @@ pub struct Info {
     #[serde(default)]
     pub md5sum: Option<String>,
     #[serde(default)]
-    pub length: Option<i64>,
+    pub length: u64,
     #[serde(default)]
     pub files: Option<Vec<TFile>>,
     #[serde(default)]
@@ -38,7 +38,7 @@ pub struct Info {
 pub struct Torrent {
     pub info: Info,
     #[serde(default)]
-    pub announce: Option<String>,
+    pub announce: String,
     #[serde(default)]
     pub nodes: Option<Vec<Node>>,
     #[serde(default)]
@@ -57,3 +57,23 @@ pub struct Torrent {
     #[serde(rename = "created by")]
     pub created_by: Option<String>,
 }
+
+pub(crate) struct Announce {
+    pub info_hash: String,
+    pub peer_id: String,
+    /// The port on which we are listening.
+    pub port: u16,
+    /// True IP address
+    pub ip: Option<IpAddr>,
+    /// Number up bytes downloaded so far.
+    pub downloaded: u64,
+    /// Number up bytes uploaded so far.
+    pub uploaded: u64,
+    /// Number up bytes left to download.
+    pub left: u64,
+    pub peer_count: Option<usize>,
+    #[allow(dead_code)]
+    pub tracker_id: Option<String>,
+}
+
+//https://github.com/mandreyel/cratetorrent/blob/master/cratetorrent/src/tracker.rs
